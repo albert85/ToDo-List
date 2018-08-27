13d9b98722933840a63b1ecc9ffe3e28618971b2
Olarewaju Temtope Albert <tmtpalbert@gmail.com>
2018-08-27 16:37:48 +0100

    Refactor code base

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,36 @@
 
-import React, { Component } from "react";
+import React, { Component, Fragment } from "react";
 import "./App.css";
 import Navbar from "./components/navbar";
-import Main from "./components/main";
+import AddTodo from "./components/AddTodo";
 class App extends Component {
   render() {
     return (
-      <React.Fragment>
-        <Navbar />
-        <main className="container">
-          <Main />
-        </main>
-      </React.Fragment>
+      <Fragment>
+        <Navbar/>
+        <div className="container">
+        <div className="row">
+          <div className="mt-6 col-4">
+            <AddTodo/>
+          </div>
+          <div className="mt-4 col-8">
+            <div className="task_list box-shadow rounded">
+              <div class="inner rounded">
+                <div className="text-white pl-4 tasklist">
+                  <span>
+                    <i class="fa fa-tasks" />
+                  </span>
+                  <div>
+                  <h3>Tasks</h3>
+
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+      </Fragment>
     )
   }
 }
--- src/components/AddTodo.js
+++ src/components/AddTodo.js
@@ -1,41 +1,15 @@
-import React, { Component } from 'react';
-import ListTodo from './ListTodo.js';
+import React, { PureComponent } from 'react';
 import '../App.css';
 
-class AddTodo extends Component {
-
-    constructor(props) {
-    super(props);
-    this.state = {
-      term: '',
-      items: []
-    };
-  }
-
-  onChange = (event) => {
-    event.preventDefault();
-    this.setState({ term: event.target.value });
-  }
-
-  onSubmit = (event) => {
-    event.preventDefault();
-    this.setState({
-      term: '',
-      items: [...this.state.items, this.state.term]
-    });
-  }
+class AddTodo extends PureComponent {  
 
   render() {
     return (
       <div>
         <form className="App" onSubmit={this.onSubmit}>
-          <input value={this.state.term} 
-                 onChange={this.onChange} 
-                 placeholder="Add task" 
-          />
+          <input placeholder="Add task" />
           <button>Add New Task.</button>
         </form>
-        <ListTodo items={this.state.items} />
       </div>
     );
   }
